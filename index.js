104339f534667422834976392b9f8a11588cd2b9
Tashiro0927 <120367778+Tashiro0927@users.noreply.github.com>
2022-12-12 17:20:38 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,99 @@
+'use strict';
+
+const express = require('express');
+const line = require('@line/bot-sdk');
+const PORT = process.env.PORT || 3000;
+
+const config = {
+    channelSecret: process.env.CHANNEL_SECRET,
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+};
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello LINE BOT!(GET)')); //ブラウザ確認用(無くても問題ない)
+app.post('/webhook', line.middleware(config), (req, res) => {
+    console.log(req.body.events);
+
+    //ここのif分はdeveloper consoleの"接続確認"用なので削除して問題ないです。
+    if(req.body.events[0].replyToken === '00000000000000000000000000000000' && req.body.events[1].replyToken === 'ffffffffffffffffffffffffffffffff'){
+        res.send('Hello LINE BOT!(POST)');
+        console.log('疎通確認用');
+        return; 
+    }
+
+    Promise
+      .all(req.body.events.map(handleEvent))
+      .then((result) => res.json(result));
+});
+
+const client = new line.Client(config);
+
+async function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: event.message.text //実際に返信の言葉を入れる箇所
+  });
+}
+
+app.listen(PORT);
+console.log(`Server running at ${PORT}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function reply(e) {
+  
+  var channel_access_token = "massaging apiから提供されるchannel_access_token";
+  var message = {
+    "replyToken" : e.replyToken,
+    "messages" : [
+      {
+        "type" : "text",
+        "text" : ((e.message.type=="text") ? e.message.text : "Text以外は返せません・・・")
+      }
+    ]
+  };
+
+  var replyData = {
+    "method" : "post",
+    "headers" : {
+      "Content-Type" : "application/json",
+      "Authorization" : "Bearer " + channel_access_token,
+    },
+    "payload" : JSON.stringify(message)
+  };
+  addLog(replyData);
+  var response = UrlFetchApp.fetch("https://api.line.me/v2/bot/message/reply", replyData);
+  
+  return response.getResponseCode();
+}
